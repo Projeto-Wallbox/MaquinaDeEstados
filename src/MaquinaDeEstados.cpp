e2c19ca0a0eb01566e049766f0aae80d184f4400
Valdemir_Miranda <valdemir14msp@gmail.com>
2023-12-21 18:03:34 -0300

    Ajuste no cálculo da razão cíclica
    Foi realizado ajustes para determinar o valor da razão cíclica e outros pontos da chargingStationMain

--- src/MaquinaDeEstados.cpp
+++ src/MaquinaDeEstados.cpp
@@ -1,19 +1,19 @@
+//***** Bibliotecas *****//	
 #include <stdio.h>
 #include <stdlib.h>
 #include "driver/gpio.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
 #include "MaquinaDeEstados.h"
-
 GlobalStruct Dados; //Inicializa estrutura de dados (VER MELHOR FORMA)
 
 //Funcao que deve ser chamada na Interrupcao
 int funcaoInterrupcao()
 { // Esta funcao e chamada a cada 16,6 us
 	// Variaveis para os contadores das interrupcoes
-	static int funcao_principal = 0;
-	static int measure_two = 0;
-	static int printar = 0;
+	static int cont_principal = 0;
+	static int cont_atualiza = 0;
+	static int cont_interfaceUsuario = 0;
 
 	// Variaveis da Leitura do Piloto
 	static int medida_piloto = 4000;
@@ -27,9 +27,9 @@ int funcaoInterrupcao()
 	static int corrente_maxima;
 
 	// measure_one++;
-	funcao_principal++;
-	measure_two++;
-	printar++;
+	cont_principal++;
+	cont_atualiza++;
+	cont_interfaceUsuario++;
 
 	// a cada 166 us (6kHz)
 	medida_piloto = adc1_get_raw(ADC1_CHANNEL_4);	 // Leitura do piloto (1).																			
@@ -37,27 +37,29 @@ int funcaoInterrupcao()
 
 	Dados.Estado_Veiculo = estado_veiculo;  //atualiza estado na struct
 
-	if (funcao_principal >= 6) // a cada 1 ms (1kHz)
+	if (cont_principal >= 6) // a cada 1 ms (1kHz)
 	{
 		//TESTE DE LEITURA DO ESTADO E0
 	  if(Dados.Estado_Veiculo == 0){
-			printf("ACIONAR BATERIA");   /// em teste
+			//printf("ACIONAR BATERIA");   /// em teste
 		}
 
-		funcao_principal = 0;
+		cont_principal = 0;
 		medida_proximidade = adc1_get_raw(ADC1_CHANNEL_5);     // Faz a leitura analogica do proximidade (3)
 		cabo_conectado = correnteCabo(medida_proximidade);		 // Identificacao do Cabo (4)
 		estado_veiculo = defineEstado(media_piloto);           // Determina o Estado (5)
 	
-		if(Dados.Corrente_Usuario < cabo_conectado){ //logica de maior corrente suportada 
+		if(Dados.Corrente_Usuario < cabo_conectado && Dados.Corrente_Usuario<=32){ //logica de maior corrente suportada 
 			corrente_maxima = Dados.Corrente_Usuario;
-		}else{corrente_maxima = cabo_conectado;}
+		}else if(cabo_conectado<32){
+			corrente_maxima = cabo_conectado;
+			}else{corrente_maxima = 32;}
 	
 		razao_ciclica = chargingStationMain(estado_veiculo, corrente_maxima); // MAQUINAS DE ESTADOS, CONTATORA E CALCULO DA RAZAO CICLICA    (6 e 7)
 	}
 	else
 	{
-		if (measure_two >= 600) // a cada 100 ms ( 10 Hz)
+		if (cont_atualiza >= 600) // a cada 100 ms ( 10 Hz)
 		{
 			//Atuzaliza os dados da estrura "maquinaDeEstados"
 			leBotao();
@@ -66,15 +68,15 @@ int funcaoInterrupcao()
 			Dados.Razao_Ciclica = razao_ciclica;
 			Dados.Media_Piloto = media_piloto;
 
-			measure_two = 0;
+			cont_atualiza = 0;
 		}
 		else
 		{
-			if (printar >= 6000) // a cada 1000 ms (1 Hz)
+			if (cont_interfaceUsuario >= 6000) // a cada 1000 ms (1 Hz)
 			{ 
 				acendeLed();
 				printTela();
-				printar = 0;
+				cont_interfaceUsuario = 0;
 			}
 		}
 	}
@@ -83,7 +85,7 @@ int funcaoInterrupcao()
 }
 
 //Funcao que calcula media dos sinais lidos no AD Piloto
-int positivaPiloto(int Piloto)
+int positivaPiloto(int piloto)
 {
 	// printf("P_Piloto\n");
 	static int j = 0;											 // L�gica de determina��o do valor m�ximo de um per�odo
@@ -103,35 +105,35 @@ int positivaPiloto(int Piloto)
 	else
 	{
 		j++;
-		if ((Piloto > media[0]) || (Piloto > media[1]) || (Piloto > media[2]) || (Piloto > media[3]) || (Piloto > media[4]))
+		if ((piloto > media[0]) || (piloto > media[1]) || (piloto > media[2]) || (piloto > media[3]) || (piloto > media[4]))
 		{
-			if (Piloto > media[0])
+			if (piloto > media[0])
 			{
-				media[0] = Piloto + 1;
+				media[0] = piloto + 1;
 			}
 			else
 			{
-				if (Piloto > media[1])
+				if (piloto > media[1])
 				{
-					media[1] = Piloto + 1;
+					media[1] = piloto + 1;
 				}
 				else
 				{
-					if (Piloto > media[2])
+					if (piloto > media[2])
 					{
-						media[2] = Piloto + 1;
+						media[2] = piloto + 1;
 					}
 					else
 					{
-						if (Piloto > media[3])
+						if (piloto > media[3])
 						{
-							media[3] = Piloto + 1;
+							media[3] = piloto + 1;
 						}
 						else
 						{
-							if (Piloto > media[4])
+							if (piloto > media[4])
 							{
-								media[4] = Piloto + 1;
+								media[4] = piloto + 1;
 							}
 							else
 							{
@@ -201,11 +203,11 @@ int correnteCabo(int proximidade) {
 					{
 						if(proximidade>(3600))													//if(proximidade>(((4499*3.3)/(4500+470))*(4095/3.35)))
 						{
-							corrente_cabo=0;
+							corrente_cabo = 0;
 						}
 						else
 						{
-							corrente_cabo=0;
+							corrente_cabo = 0;
 						}
 					}
 				}
@@ -269,166 +271,126 @@ int defineEstado(int media_x1)
 }
 
 //Funcao que contém a logica de comutacao do dispositivo de manobra
-int chargingStationMain(int estado, int corrente_maxima)
-{
-  static int razao = 1000;                // Razao ciclica do PWM
-  static int k = 0;                       // Contador do bloqueio da contatora
-  static int l = 0;                       // Gerador da interrupcaoo de 1 kHz
-  static int m = 0;                       // Contador do bloqueio da razao ciclica
-  static int corrente_da_estacao = 0;     // Corrente maxima que a estacaoo ira fornecer
-  static int contatora = 0;               // Estado da contatora
-  static int esta_carregando = 0;         // detecta se_ a estacaoo de recarga esta carregando
-  static int bloqueio_contatora = 0;      // bloqueia a contatora por 6 segundos caso entre no modo ventilacaoo
-  static int bloqueio_razao_ciclica = 0;  // Variavel que bloqueia a alteracao da razao ciclica por 5 segundos
-  //static int abrir_dispositvo_erroS2 = 0; // finalizar_recarga Abrir dispositivo de manobra (Transicao 10.2)
+int chargingStationMain(int estado, int corrente_cabo)
+{	//Entra nessa lógica 1 vez a cada ciclo do PWM (1 kHz)
+	
+	static int razao=1000;											//Razão cíclica do PWM
+	static int k=0;															//Contador do bloqueio da contatora
+	static int m=0;												      //Contador do bloqueio da razão cíclica
+	static int corrente_da_estacao=0;			      //Corrente máxima que a estaçao irá fornecer
 	static int cont = 0;
-  
-  Dados.Estacao_Carregando = contatora; 								//Atualiza esta informacao na estrutura;
-  
-  //***************Codigo da Corrente na estacao e da contatora*************************
-  //ETAPA DA LOGICA DO ACIONAMENTO DA CONTATORA - **fazer uma logica de acionamento para os reles
-  if ((estado == 0) || (estado == -12) || (corrente_maxima == 0))//mantem dispositivo de manobra desligado
-  {
-    corrente_da_estacao = 0; 
-    contatora = 0;
-		dispositivoDeManobra(contatora);
-    esta_carregando = 0;
-    if ((estado == 0) || (estado == -12))  //mantem dispositivo de manobra desligado
-    {
-      bloqueio_contatora = 0;
-      k = 0;
-    }
-  }
-  else
-  {
-    if ((estado == 6) && (esta_carregando == 1)) //Fecha dispositivo de manobra
-    {
-      bloqueio_contatora = 1;
-      contatora = 1;
-    }
-    if ((bloqueio_contatora == 1) && (estado == 3))
-    {
-      corrente_da_estacao = 0;
-      contatora = 1;
-      dispositivoDeManobra(contatora);
-      if (l == 1)
-      {
-        k = k + l;
-      }
-      esta_carregando = 0;
-      bloqueio_contatora = 1;
-      if (k >= 60)
-      { // <---------Deveria ser 6000
-        bloqueio_contatora = 0;
-        k = 0;
-      }
-    }
+	static bool estado_F=false;									//Variével que altera o valor da razão cíclica para 0
+	static bool estadoDispositivoManobra=false;	//Estado do dispositivo de manobra
+	static bool bloqueio_contatora=false;				//Variável que bloqueia a contatora por 6 segundo caso entre no modo ventilação
+	static bool bloqueio_razao_ciclica=false;		//Variável que bloqueia a alteração da razão cíclica por 5 segundos
+	static bool iniciar_recarga=false;					//Variável que bloqueia a alteração da razão cíclica por 5 segundos
 
-    // Logica da corrente da estacaoo e da contatora
-    else
-    {
-      k = 0;
-      if (((estado == 12) || (estado == 9) || (estado == 6)) && ((corrente_maxima == 13) || (corrente_maxima == 20) || (corrente_maxima == 32) || (corrente_maxima == 63)))
-      {
-        if (estado==6)
-        {
-					if(razao>=100 && razao<1000 && Dados.Iniciar_Recarga == 1){ //ESTADO C2
-						contatora = 1;
-         		dispositivoDeManobra(contatora);
-         		esta_carregando = 1;
-						cont=0;
-
-					}else{//ESTADO C1
-						if(cont >= 6000){  //min 6S
-							contatora = 0;
-							dispositivoDeManobra(contatora);
-						}else{
+	estado_F = gpio_get_level(GPIO_NUM_23);
+	iniciar_recarga = Dados.Iniciar_Recarga;
+	Dados.Contador = cont;
+//******Lógica que decide qual será a corrente máxima da estação e o estado da contatora*********************
+	//Lógica para os Estados: A, B, E e F ou cabo desconectado
+	if((estado==12)||(estado==9)||(estado==0)||(estado==-12)||(corrente_cabo==0))			
+	{
+		estadoDispositivoManobra=false;
+		dispositivoDeManobra(estadoDispositivoManobra);
+		bloqueio_contatora=false;
+		k=0;
+		cont = 0;
+		if(estado==9)		//Se Estado B ative o PWM
+		{
+			corrente_da_estacao=corrente_cabo;
+		}
+		else
+		{
+			corrente_da_estacao=0;
+		}
+	}
+	//Lógica para os Estados C e D com cabo conectado
+	else
+	{	
+		//Lógica para o Estado C
+		if(estado==6)
+		{
+			k=0;
+			corrente_da_estacao=corrente_cabo;
+			if(razao!=1000 && iniciar_recarga == 1)			//A contatora não pode fechar antes de o PWM indicar a estação como pronta
+			{
+				cont = 0;
+				bloqueio_contatora=true;
+				estadoDispositivoManobra=true;
+				dispositivoDeManobra(estadoDispositivoManobra);
+			}else{                // Lógica de abertura da contatora, não responta ao término da recarga (Transição 10.2)
+				if(cont >= 6000){   //Minimo 6 segundos
+							estadoDispositivoManobra = 0;
+							dispositivoDeManobra(estadoDispositivoManobra);
+					}else{
 							cont++;
-						}
-						//Dados.Iniciar_Recarga = 0;
-						//contatora = 0;
-         		//dispositivoDeManobra(contatora);
-					} 
-
-					if(Dados.Iniciar_Recarga == 0){  //Razao de 100% quando ir para o estado A ou quando iniciar_recarga esta "desligado"
-            razao = 1002;
-          }
+					}
+			}
+		}
+		//Lógica para o Estado D
+		else
+		{
+			//Lógica para transição do Estado C para o D com a contatora já fechada
+			if((bloqueio_contatora==true)&&(estado==3))
+			{
+				k++;
+				corrente_da_estacao=0;
+				estadoDispositivoManobra=true;
+				dispositivoDeManobra(estadoDispositivoManobra);
+				if(k>=6000)			//Lógica para fazer esperar 6 segundos após a transição do Estado C para o D com a contatora já fechada
+				{
+					bloqueio_contatora=false;
+					k=0;
 				}
-        else
-        {//ALTERADO
-          if((estado == 12)||(Dados.Iniciar_Recarga == 0)){  //Razao de 100% quando ir para o estado A ou quando iniciar_recarga esta "desligado"
-            razao = 1002;
-						Dados.Iniciar_Recarga = 0;
-          }
-          contatora = 0;
-					dispositivoDeManobra(contatora);
-          esta_carregando = 0;
-					cont = 0;                             //contador relacionado a transicao 10.2
-        }
-        corrente_da_estacao = corrente_maxima; 
-      }
+			}
+			//Lógica para 6 segundos após a transição do Estado C para o D ou contatora aberta durante a transição
 			else
-      {
-        //corrente_da_estacao = 0;
-        contatora = 0;
-				dispositivoDeManobra(contatora);
-        esta_carregando = 0;
-      }
-    }
-  }
+			{
+				k=0;
+				bloqueio_contatora=false;
+				corrente_da_estacao=0;
+				estadoDispositivoManobra=false;
+				dispositivoDeManobra(estadoDispositivoManobra);
+			}
+		}
+	}
 
-	Dados.Contador = cont;//Só para msotar o estado da variável cont
+	//************Codificação da corrente máxima da estação através da razão cíclica do PWM***********************
+	//Lógica caso a estação não esteja pronta para fornecer energia
+	if(corrente_da_estacao==0 || iniciar_recarga == 0)
+	{
+		razao=1000;
+	}
+	
+	//Lógica do bloqueio da razão cíclica
+	if(bloqueio_razao_ciclica==true)
+	{
+		m++;
+		if(m>=5000)
+		{
+			bloqueio_razao_ciclica=false;
+			m=0;
+		}
+	}
 
+	//Lógica caso a razão cíclica deva ser atualizada
+	if(bloqueio_razao_ciclica==false)
+	{	
+		if(corrente_da_estacao>=6 && corrente_da_estacao<=32){
+				razao = (corrente_da_estacao/0.6)*(1023/100);
+		}else{razao = 1000;}
+		
+		bloqueio_razao_ciclica=true;
+	}
 
-  // *************************** Codigo do Gerado PWM *************************************
-  // LOGICA DO BLOQUEIO DA RAZAO CICLICA (SEQUENCIA 6)
-  if ((bloqueio_razao_ciclica == 1) && (l == 1)) //para nao alterar a razao ciclica em menos de 5s apos ter feito uma atualizacao 
-  {
-    m = m + l;
-    if (m >= 50)
-    { 
-      bloqueio_razao_ciclica = 0;
-      m = 0;
-    }
-  }
+	//Mantém a razão cíclica em 0%, enquando o erro persistir
+	if(estado_F == true){
+		razao = 0;
+	}
 
-  // Logica de determinacao da largura de pulso
-  if ((l == 1) && (bloqueio_razao_ciclica == 0))
-  { 
-    bloqueio_razao_ciclica = 1;
-    //razao = (corrente_da_estacao/0.6)*(1023/100);
-    switch (corrente_da_estacao)
-    {
-      //case 63:
-       // razao = 533;
-        //break;
-      case 32:
-        razao = 533;
-        break;
-      case 20:
-        razao = 333;
-        break;
-      case 13:
-        razao = 216; // razao=(0.13/0.6)*1000;
-        break;
-      case 6:
-        razao = 100; 
-        break;
-      default:
-        razao = 1000;//1000;
-        break;
-    }
-  }
-
-  if (l >= 100)
-  {
-    l = 0;
-  }
-
-  // Logica da interrupcao do PWM
-  l = l + 1;
-
-  return razao;
+	return razao;
 }
 
 //Funcao para controle dos led indicadores
@@ -468,7 +430,7 @@ void leBotao(){
         Dados.Iniciar_Recarga = 1;
     }
 
-    if (cont >= 50) {
+    if (cont >= 30) {
         Dados.Iniciar_Recarga = 0;
     }
 }
